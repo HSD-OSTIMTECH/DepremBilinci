930c5987ee169849016d872b2ecbeba531f7e101
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-05 10:27:29 +0300

    feat: done navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,14 @@
 import React from 'react'
+import { IoIosArrowDown } from "react-icons/io";
+
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const Navbar = () => {
   return (
@@ -11,8 +21,68 @@ const Navbar = () => {
 
         <div className='flex items-center gap-6 font-medium'>
             <a href="/">Ana Sayfa</a>
-            <a href="/deprembilinci">Deprem Bilinci</a>
-            <a href="/katkı">Nasıl Katkı Sağlarsınız</a>
+
+            <DropdownMenu>
+
+                <DropdownMenuTrigger className='flex items-center gap-2 cursor-pointer'>
+                    <span>Deprem Eğitimleri</span>
+                    <IoIosArrowDown />
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent>
+
+                    <DropdownMenuItem>
+                        <a href="/cocuk-egitim">Çocuklar</a>
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem>
+                        <a href="/ebeveyn-egitim">Ebeveynler</a>
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem>
+                        <a href="/ogrenci-egitim">Öğrenciler</a>
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem>
+                        <a href="/ogretmen-egitim">Öğretmenler</a>
+                    </DropdownMenuItem>
+
+                </DropdownMenuContent>
+
+            </DropdownMenu>
+
+            <DropdownMenu>
+
+                <DropdownMenuTrigger className='flex items-center gap-2 cursor-pointer'>
+                    <span>Nasıl Katkı Sağlarsınız</span>
+                    <IoIosArrowDown />
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent>
+
+                    <DropdownMenuItem>
+                        <a href="/katkı/genel-rehber">Genel Rehber</a>
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem>
+                        <a href="/katkı/yazılım">Yazılım</a>
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem>
+                        <a href="/katkı/tasarım">Tasarım</a>
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem>
+                        <a href="/katkı/icerik-uret">İçerik Üret</a>
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem>
+                        <a href="/katkı/paylas">Paylaş</a>
+                    </DropdownMenuItem>
+
+                </DropdownMenuContent>
+
+            </DropdownMenu>
 
             <a href="/iletisim" className='px-6 py-2 rounded-sm bg-primary text-background hover:-translate-y-0.5 transition-all'>İletişim</a>
         </div>
