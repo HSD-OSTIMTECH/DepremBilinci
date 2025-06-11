a28dc7a7a5de5a8c4321c73ac894f6b5a577265e
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-11 23:06:41 +0300

    feat: updated tesler ui

--- pages/egitim/testler.tsx
+++ pages/egitim/testler.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { PiStudentBold, PiChalkboardTeacherBold, PiUsersThreeBold } from "react-icons/pi";
+import { FaChild } from "react-icons/fa";
 import sorularData from "../../sorular.json"; // yolu senin yapına göre düzenle
 
 // Kategori tipleri
@@ -10,11 +12,11 @@ type Soru = {
   dogru: string;
 };
 
-const kategoriler: { id: Kategori; label: string }[] = [
-  { id: "ogrenci", label: "Öğrenci" },
-  { id: "ogretmen", label: "Öğretmen" },
-  { id: "ebeveyn", label: "Ebeveyn" },
-  { id: "cocuk", label: "Çocuk" }
+const kategoriler: { id: Kategori; label: string; icon: React.ReactNode; link: string; desc: string }[] = [
+  { id: "ogrenci", label: "Öğrenciler", icon: <PiStudentBold className="text-3xl mb-3" />, link: "/egitim/ogrenciler", desc: "için hazırlanmış testler" },
+  { id: "ogretmen", label: "Öğretmenler", icon: <PiChalkboardTeacherBold className="text-3xl mb-3" />, link: "/egitim/ogretmenler", desc: "için hazırlanmış testler" },
+  { id: "ebeveyn", label: "Ebeveynler", icon: <PiUsersThreeBold className="text-3xl mb-3" />, link: "/egitim/ebeveynler", desc: "için hazırlanmış testler" },
+  { id: "cocuk", label: "Çocuklar", icon: <FaChild className="text-3xl mb-3" />, link: "/egitim/cocuklar", desc: "için hazırlanmış testler" }
 ];
 
 // Diziyi karıştıran yardımcı fonksiyon
@@ -50,20 +52,42 @@ const Testler: React.FC = () => {
   };
 
   return (
-    <div className="px-4 py-36 max-w-4xl mx-auto text-center">
-      <h2 className="text-2xl font-semibold text-primary mb-6">Kategori Seç</h2>
+    <div className="py-16">
+      <section className="mb-10">
+        <h1 className="text-2xl font-semibold mb-4 text-primary">Kendinizi Test Edin</h1>
+        <p className="mb-4 text-neutral-700 max-w-2xl">
+          Eğer eğitimleri incelediyseniz, şimdi öğrendiklerinizi test etme zamanı! Aşağıdaki kategori kartlarına tıklayarak ilgili testleri başlatabilirsiniz. Her kategori için rastgele sorular hazırlanmıştır.
+          Testler, deprem güvenliği ve hazırlığı konusundaki bilginizi ölçmenize yardımcı olacak. Her test sonunda doğru cevapları görebileceksiniz.
+        </p>
+      </section>
 
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 mt-16">
-        {kategoriler.map((kat) => (
-          <button
-            key={kat.id}
-            onClick={() => kategoriSecildi(kat.id)}
-            className="w-48 h-48 px-4 py-4 border cursor-pointer border-primary text-primary rounded-md hover:bg-primary hover:text-white transition text-lg font-medium"
-          >
-            {kat.label}
-          </button>
-        ))}
-      </div>
+      <section className="py-8">
+        <h2 className="text-2xl font-semibold text-primary mb-2">Test Kataloğumuz</h2>
+        <p className="text-neutral-700 mb-8">
+          Temel afet bilgileri, çanta hazırlığı ve deprem bilinci hakkında mini testler.
+        </p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+          {kategoriler.map((kat) => (
+            <div
+              key={kat.id}
+              className="border border-neutral-400 rounded-lg p-6 flex flex-col items-center"
+            >
+              {kat.icon}
+              <h3 className="text-lg font-semibold text-primary">{kat.label}</h3>
+              <p className="text-neutral-700 mb-4">
+                {kat.desc}{" "}
+              </p>
+              <button
+                onClick={() => kategoriSecildi(kat.id)}
+                className="mt-auto px-4 py-2 rounded bg-primary text-background font-semibold hover:-translate-y-0.5 transition cursor-pointer"
+              >
+                Teste Başla
+              </button>
+            </div>
+          ))}
+        </div>
+      </section>
 
       {/* Modal */}
       {modalAcik && (
@@ -71,7 +95,7 @@ const Testler: React.FC = () => {
           <div className="bg-white rounded-lg p-6 w-full max-w-lg shadow-lg relative max-h-[90vh] overflow-y-auto">
             <button
               onClick={modalKapat}
-              className="absolute top-2 right-3 text-gray-500 hover:text-red-500 text-xl"
+              className="absolute top-2 right-3 text-neutral-500  max-w-2xlhover:text-red-500 text-xl"
             >
               &times;
             </button>
@@ -99,4 +123,4 @@ const Testler: React.FC = () => {
   );
 };
 
-export default Testler;
+export default Testler
